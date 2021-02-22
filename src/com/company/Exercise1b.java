9014e7bc715448ffb9d8fc3b6081f77a4b2d51bd
pozdnek <79456102+pozdnek@users.noreply.github.com>
2021-02-22 20:06:23 +0300

    Initial commit

---
+++ src/com/company/Exercise1b.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Exercise1b {
+
+    public static void main(String[] args) {
+        int x = 12;
+        while (x > 2){
+            x = x - 1;
+            if (x < 15){
+                System.out.println("большой икс");
+            }
+        }
+    }
+}
